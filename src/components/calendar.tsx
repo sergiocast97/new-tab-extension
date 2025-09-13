f6453a1904ea233cb72aedabb9d947604e1cfc83
Sergio <sergiocast97@gmail.com>
2025-09-13 17:28:39 +0100

    fixed: moved util functions outside component for optimization

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,25 +1,26 @@
+const getMonthName = (date: Date) => {
+  return date.toLocaleString('en-UK', {
+    month: 'long',
+  });
+};
+
+const getOrdinal = (number: number): string => {
+  const mod10 = number % 10;
+  const mod100 = number % 100;
+  let suffix = 'th';
+
+  if (mod10 === 1 && mod100 !== 11) {
+    suffix = 'st';
+  } else if (mod10 === 2 && mod100 !== 12) {
+    suffix = 'nd';
+  } else if (mod10 === 3 && mod100 !== 13) {
+    suffix = 'rd';
+  }
+
+  return `${number}${suffix}`;
+};
+
 export default function Calendar({ date }: { date: Date }) {
-  const getMonthName = (date: Date) => {
-    return date.toLocaleString('en-UK', {
-      month: 'long',
-    });
-  };
-
-  const getOrdinal = (number: number): string => {
-    const mod10 = number % 10;
-    const mod100 = number % 100;
-    let suffix = 'th';
-
-    if (mod10 === 1 && mod100 !== 11) {
-      suffix = 'st';
-    } else if (mod10 === 2 && mod100 !== 12) {
-      suffix = 'nd';
-    } else if (mod10 === 3 && mod100 !== 13) {
-      suffix = 'rd';
-    }
-
-    return `${number}${suffix}`;
-  };
   return (
     <>
       <p className="text-lg sm:text-xl font-semibold text-theme-white drop-shadow-md">
