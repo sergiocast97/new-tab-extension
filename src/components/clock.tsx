68b61e7f2600ce0e66f5912098e76847968f8089
Sergio <sergiocast97@gmail.com>
2025-09-13 17:28:21 +0100

    fix: refactored two digits conversion

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,13 +1,9 @@
-export default function Clock({ date }: { date: Date }) {
-  const toTwoDigits = (value: number) => {
-    return value.toLocaleString('en-UK', {
-      minimumIntegerDigits: 2,
-    });
-  };
+const toTwoDigits = (n: number) => n.toString().padStart(2, '0');
 
+export default function Clock({ date }: { date: Date }) {
   return (
     <>
-      <p className="font-semibold text-theme-white text-9xl/none drop-shadow-md ">
+      <p className="font-semibold text-theme-white text-8xl/none sm:text-9xl/none drop-shadow-md ">
         <span>{toTwoDigits(date.getHours())}</span>
         <span className="animate-separator">:</span>
         <span>{toTwoDigits(date.getMinutes())}</span>
